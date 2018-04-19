d2f1d6dda47751f5d9da9422d4e5ff0ab2306e27
seis <xiaonanln@gmail.com>
2018-04-20 01:32:41 +0800

    experimenting ...

--- haas/__init__.py
+++ haas/__init__.py
@@ -6,5 +6,6 @@ def hello():
 
 from .haas import Register, Run
 from .Service import Service
+from .etcdman import SetEtcdAddress
 
-__all__ = ['Service', 'Register', 'Run']
+__all__ = ['Service', 'Register', 'Run', 'SetEtcdAddress']
--- sample/app.py
+++ sample/app.py
@@ -9,6 +9,7 @@ def main():
     haas.Register(dummy.DummyService1)
     haas.Register(dummy.DummyService2)
     haas.Register(web.WebService)
+    haas.SetEtcdAddress( '127.0.0.1', 2379 )
     haas.Run()
 
 if __name__ == '__main__':
